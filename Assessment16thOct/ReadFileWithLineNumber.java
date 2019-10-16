3c675e527f8d12510d8ad9a4b3f727102e0d4e38
Indrijeet Kumar <kumarindrijeet2042@gmail.com>
2019-10-16 10:49:44 +0530

    Add files via upload

---
+++ Assessment16thOct/ReadFileWithLineNumber.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class ReadFileWithLineNumber{
+	public static void main(String[] args) throws IOException {
+		File file=new File("file12.txt");
+		StringBuilder sb=new StringBuilder(" ");
+		BufferedReader br=new BufferedReader(new FileReader(file));
+		String str=" ";
+		int i=1;
+		while((str=br.readLine())!=null)
+		{
+			System.out.print(i);
+			sb.append(str);
+			System.out.println("Constents of the file is:"+br.readLine());
+			i++;
+		}
+		br.close();
+		//System.out.println("Constents of the file is:"+sb);
+	}
+}
